63dc88715a24e7e88456316d2591506084598a8f
kig9981 <ingu9981@naver.com>
2024-04-17 20:23:47 +0900

    Style: flake8 적용

--- tests/NonogramServer/test_views.py
+++ tests/NonogramServer/test_views.py
@@ -2,15 +2,17 @@ import pytest
 from src.NonogramServer.NonogramServer.views import get_nonogram_board
 from django.test.client import RequestFactory
 
+
 @pytest.fixture
 def mock_request():
     return RequestFactory()
+
 
 def test_get_nonogram_board(mock_request: RequestFactory):
     url = '/get_nonogram_board/'
     request = mock_request.post(
         path=url,
-        data={"board_id": 0},
+        data={"session_id": 0},
         content_type="Application/json",
     )
     response = get_nonogram_board(request)
