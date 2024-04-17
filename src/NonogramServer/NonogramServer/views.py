620eeadd9e668d1f12c66b8aa3c7608123bd184d
kig9981 <ingu9981@naver.com>
2024-04-17 20:05:10 +0900

    NonogramServer에 get_nonogram_board 기본형 및 테스트 추가

--- src/NonogramServer/NonogramServer/views.py
+++ src/NonogramServer/NonogramServer/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpRequest
+from django.http import HttpResponse
 
 # Create your views here.
+def get_nonogram_board(request: HttpRequest):
+    return HttpResponse("get_nonogram_board")
