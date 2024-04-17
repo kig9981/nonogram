1668dae0caea7ee644cf31f6ead4e05cab7edd9c
kig9981 <ingu9981@naver.com>
2024-04-17 19:20:51 +0900

    기본 nonogramserver 템플릿 생성

--- src/NonogramServer/NonogramServer/settings.py
+++ src/NonogramServer/NonogramServer/settings.py
@@ -16,14 +16,12 @@ import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-env = environ.Env()
-environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = 'django-insecure-f59fx$ylm=%qt4%49o3l%b4&of!8i547q#u*eq7tsq9a9tmypf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -77,26 +75,11 @@ WSGI_APPLICATION = 'NonogramServer.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": env("DB_NAME"),
-        "USER": env("DB_USER"),
-        "PASSWORD": env("DB_PASSWORD"),
-        "HOST": env("DB_HOST"),
-        "PORT": env("DB_PORT"),
-        # "OPTIONS": {
-        #     # "service": "nonogram_service",
-        #     "passfile": ".nonogram_pgpass",
-        # },
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 
 # Password validation
