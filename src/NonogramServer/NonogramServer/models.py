77fb093f4dc8acac2310655697121c3f3780fb29
kig9981 <ingu9981@naver.com>
2024-04-21 16:08:43 +0900

    feat: 모델 작성

--- src/NonogramServer/NonogramServer/models.py
+++ src/NonogramServer/NonogramServer/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class NonogramBoard(models.Model):
+    board_id = models.IntegerField(primary_key=True)
+    board = models.TextField(null=True)
+
+
+class Session(models.Model):
+    session_id = models.IntegerField(primary_key=True)
+    board_id = models.ForeignKey(NonogramBoard, on_delete=models.CASCADE)
+    revealed = models.TextField(null=True)
+    mark_x = models.TextField(null=True)
+    mark_question = models.TextField(null=True)
