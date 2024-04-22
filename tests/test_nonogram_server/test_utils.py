fa3e200befd1380cb97acbf7ef1ed76b6da17d08
kig9981 <ingu9981@naver.com>
2024-04-22 23:48:48 +0900

    tests: validate_gameboard 테스트코드 작성

--- tests/test_nonogram_server/test_utils.py
+++ tests/test_nonogram_server/test_utils.py
@@ -4,7 +4,68 @@ from src.NonogramServer.Nonogram.utils import serialize_gameboard
 
 
 def test_validate_gameboard():
-    pass
+    board = [
+        [1, 1],
+        [1, 1],
+    ]
+
+    try:
+        result = validate_gameboard(board)
+        assert result == True
+    except ValueError as error:
+        assert "valid gameboard: should not make exception(" + str(error) + ")" and 0
+
+    board = [
+        [1, -1],
+        [0, 1],
+    ]
+
+    try:
+        result = validate_gameboard(board)
+        assert result == False
+    except ValueError as error:
+        assert "valid gameboard: should not make exception" and 0
+
+    board = [
+        [1, -1],
+        [0, 1, 1],
+    ]
+
+    try:
+        result = validate_gameboard(board)
+        assert "invalid gameboard: should make exception" and 0
+    except ValueError as error:
+        assert "Invalid gameboard(Row length error)." == str(error)
+
+    board = 14
+
+    try:
+        result = validate_gameboard(board)
+        assert "invalid gameboard: should make exception" and 0
+    except ValueError as error:
+        assert "Invalid gameboard(Invalid type)." == str(error)
+
+    board = [
+        ["1", "1"],
+        ["1", "1"],
+    ]
+
+    try:
+        result = validate_gameboard(board)
+        assert "invalid gameboard: should make exception" and 0
+    except ValueError as error:
+        assert "Invalid gameboard(Invalid item type)." == str(error)
+
+    board = [
+        [1, -1],
+        [0, 2],
+    ]
+
+    try:
+        result = validate_gameboard(board)
+        assert "invalid gameboard: should make exception" and 0
+    except ValueError as error:
+        assert "Invalid gameboard(Invalid range(-1 ~ 1))." == str(error)
 
 
 def test_deserialize_gameboard():
