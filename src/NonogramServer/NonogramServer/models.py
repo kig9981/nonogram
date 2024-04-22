24f4d32964003f2296029e5d55bc92abceee79d8
kig9981 <ingu9981@naver.com>
2024-04-22 23:32:07 +0900

    fix: NonogramBoard모델 구조 변경 및 유효성 검사 로직 구현

--- src/NonogramServer/NonogramServer/models.py
+++ src/NonogramServer/NonogramServer/models.py
@@ -1,9 +1,25 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from Nonogram.utils import deserialize_gameboard
+
 
 # Create your models here.
 class NonogramBoard(models.Model):
     board_id = models.IntegerField(primary_key=True)
     board = models.TextField(null=True)
+    num_row = models.IntegerField(default=5)
+    num_column = models.IntegerField(default=5)
+    theme = models.CharField(max_length=20, default="")
+
+    def clean(self):
+        try:
+            board = deserialize_gameboard(self.board)
+        except ValueError as error:
+            raise ValidationError("Invalid board data (" + str(error) + ")")
+        row, column = len(board), len(board[0])
+
+        if row is not self.num_row or column is not self.num_column:
+            raise ValidationError("Mismatch between provided row and column counts and actual board data")
 
 
 class Session(models.Model):
