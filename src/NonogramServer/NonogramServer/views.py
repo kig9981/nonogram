bac18bb8550d5e80739669d19a7a8004fd26e002
kig9981 <ingu9981@naver.com>
2024-04-25 00:18:31 +0900

    refactor: flake8 적용

--- src/NonogramServer/NonogramServer/views.py
+++ src/NonogramServer/NonogramServer/views.py
@@ -3,6 +3,7 @@ from django.http import HttpRequest
 from django.http import HttpResponse
 from .models import NonogramBoard
 
+
 # Create your views here.
 def get_nonogram_board(request: HttpRequest):
     # NonogramBoard.objects.get(pk=-1)
