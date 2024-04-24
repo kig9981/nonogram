94b970f6b44041baa6d37bbd2bf650a15e8b9c6f
kig9981 <ingu9981@naver.com>
2024-04-25 01:10:31 +0900

    feat: History model 추가

--- src/NonogramServer/NonogramServer/models.py
+++ src/NonogramServer/NonogramServer/models.py
@@ -3,6 +3,13 @@ from django.core.exceptions import ValidationError
 from Nonogram.utils import deserialize_gameboard
 
 
+class MoveType(models.IntegerChoices):
+    NOT_SELECTED = 0
+    REVEALED = 1
+    MARK_X = 2
+    MARK_QUESTION = 3
+
+
 # Create your models here.
 class NonogramBoard(models.Model):
     board_id = models.IntegerField(primary_key=True)
@@ -12,6 +19,7 @@ class NonogramBoard(models.Model):
     theme = models.CharField(max_length=20, default="")
 
     def clean(self):
+        super().clean()
         try:
             board = deserialize_gameboard(self.board)
         except ValueError as error:
@@ -24,7 +32,17 @@ class NonogramBoard(models.Model):
 
 class Session(models.Model):
     session_id = models.IntegerField(primary_key=True)
-    board_id = models.ForeignKey(NonogramBoard, on_delete=models.CASCADE)
+    current_game_id = models.ForeignKey("History", on_delete=models.CASCADE, null=True)
+    board_id = models.ForeignKey("NonogramBoard", on_delete=models.CASCADE)
     revealed = models.TextField(null=True)
     mark_x = models.TextField(null=True)
     mark_question = models.TextField(null=True)
+
+
+class History(models.Model):
+    gameplay_id = models.IntegerField(primary_key=True)
+    session_id = models.ForeignKey("Session", on_delete=models.CASCADE)
+    number_of_turn = models.IntegerField()
+    type_of_move = models.IntegerField(choices=MoveType)
+    x_coord = models.IntegerField()
+    y_coord = models.IntegerField()
