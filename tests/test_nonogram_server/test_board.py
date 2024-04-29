54fa3b9691d3bf3fa5566a4006a474fb8fe14400
kig9981 <ingu9981@naver.com>
2024-04-30 00:34:56 +0900

    tests: NonogramBoard.mark 테스트 코드 작성 중간 커밋

--- tests/test_nonogram_server/test_board.py
+++ tests/test_nonogram_server/test_board.py
@@ -1,6 +1,26 @@
 import pytest
-from NonogramServer.models import NonogramBoard
+from Nonogram.NonogramBoard import NonogramBoard
+from Nonogram.utils import deserialize_gameboard
+from Nonogram.utils import GameBoardCellState
+from Nonogram.utils import RealBoardCellState
 
 
+@pytest.mark.django_db
 def test_nonogram_board_mark(test_boards):
-    pass
+    for test_board in test_boards:
+        board_id = test_board["board_id"]
+        board = deserialize_gameboard(test_board["board"])
+        num_row = test_board["num_row"]
+        num_column = test_board["num_column"]
+
+        game_board = NonogramBoard(board_id)
+
+        for x in range(num_row):
+            for y in range(num_column):
+                assert game_board.mark(x, y, GameBoardCellState.MARK_X)
+                assert game_board.mark(x, y, GameBoardCellState.MARK_QUESTION)
+                assert game_board.mark(x, y, GameBoardCellState.NOT_SELECTED)
+
+                result = game_board.mark(x, y, GameBoardCellState.REVEALED)
+
+                assert (board[x][y] == RealBoardCellState.BLACK) == result
