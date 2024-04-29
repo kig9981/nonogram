c36857aec5c55ecca599d2cad60038c5abad710a
kig9981 <ingu9981@naver.com>
2024-04-30 00:36:35 +0900

    tests: 테스트 코드 완성

--- tests/test_nonogram_server/test_board.py
+++ tests/test_nonogram_server/test_board.py
@@ -24,3 +24,14 @@ def test_nonogram_board_mark(test_boards):
                 result = game_board.mark(x, y, GameBoardCellState.REVEALED)
 
                 assert (board[x][y] == RealBoardCellState.BLACK) == result
+
+                if board[x][y] == RealBoardCellState.WHITE:
+                    assert not result
+                    assert game_board.mark(x, y, GameBoardCellState.MARK_X)
+                    assert game_board.mark(x, y, GameBoardCellState.MARK_QUESTION)
+                    assert game_board.mark(x, y, GameBoardCellState.NOT_SELECTED)
+                else:
+                    assert result
+                    assert not game_board.mark(x, y, GameBoardCellState.MARK_X)
+                    assert not game_board.mark(x, y, GameBoardCellState.MARK_QUESTION)
+                    assert not game_board.mark(x, y, GameBoardCellState.NOT_SELECTED)
