d25f89a42caedf44bf59c1d1cb68545d42fbeb48
kig9981 <ingu9981@naver.com>
2024-04-30 03:30:56 +0900

    tests: get_nonogram_board에서 session_id>0, game_turn=0에 대한 테스트코드 작성

--- tests/test_nonogram_server/test_views.py
+++ tests/test_nonogram_server/test_views.py
@@ -2,6 +2,8 @@ import pytest
 import json
 from typing import Dict
 from http import HTTPStatus
+from NonogramServer.models import NonogramBoard
+from NonogramServer.models import Session
 from NonogramServer.views import get_nonogram_board
 from NonogramServer.views import set_cell_state
 from NonogramServer.views import create_new_session
@@ -39,6 +41,7 @@ def send_test_request(
 def test_get_nonogram_board(
     mock_request: RequestFactory,
     test_boards,
+    test_sessions,
     add_test_data,
 ):
     url = '/get_nonogram_board/'
@@ -77,6 +80,52 @@ def test_get_nonogram_board(
         assert response_data["board"] == test_board["board"]
         assert response_data["num_row"] == test_board["num_row"]
         assert response_data["num_column"] == test_board["num_column"]
+
+    for test_session in test_sessions:
+        GAME_NOT_START = 0
+        session_id = test_session["session_id"]
+        board_id = test_session["board_id"]
+
+        query_dict = {
+            "session_id": session_id,
+            "board_id": board_id,
+            "game_turn": GAME_NOT_START,
+        }
+        response = send_test_request(
+            mock_request=mock_request,
+            request_function=get_nonogram_board,
+            url=url,
+            query_dict=query_dict,
+        )
+
+        assert response.status_code == HTTPStatus.OK
+
+        response_data = json.loads(response.content)
+
+        board = NonogramBoard.objects.get(pk=board_id)
+        session = Session.objects.get(pk=session_id)
+
+        assert response_data["board"] == board.board
+        assert response_data["num_row"] == board.num_row
+        assert response_data["num_column"] == board.num_column
+
+        query_dict = {
+            "session_id": session_id,
+            "board_id": board_id,
+            "game_turn": -1,
+        }
+        response = send_test_request(
+            mock_request=mock_request,
+            request_function=get_nonogram_board,
+            url=url,
+            query_dict=query_dict,
+        )
+
+        latest_turn = 0 if session.current_game is None else session.current_game.current_turn
+
+        assert response.status_code == HTTPStatus.BAD_REQUEST
+
+        assert response.content.decode() == f"invalid game_turn. must be between 0 to {latest_turn}(latest turn)"
 
 
 @pytest.mark.django_db
