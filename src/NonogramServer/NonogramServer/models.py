6961c870458f6b1d5fb0cdcb7facbcf449b52fc6
kig9981 <ingu9981@naver.com>
2024-05-01 22:24:58 +0900

    feat: Session모델 mark 멤버함수 구현

--- src/NonogramServer/NonogramServer/models.py
+++ src/NonogramServer/NonogramServer/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.core.exceptions import ValidationError
 from Nonogram.utils import deserialize_gameboard
+from Nonogram.utils import deserialize_gameplay
+from Nonogram.utils import serialize_gameplay
+from Nonogram.utils import GameBoardCellState
+from Nonogram.utils import RealBoardCellState
 
 
 class MoveType(models.IntegerChoices):
@@ -30,13 +34,6 @@ class NonogramBoard(models.Model):
             raise ValidationError("Mismatch between provided row and column counts and actual board data")
 
 
-class Session(models.Model):
-    session_id = models.IntegerField(primary_key=True)
-    current_game = models.ForeignKey("History", on_delete=models.SET_DEFAULT, null=True, default=None)
-    board_data = models.ForeignKey("NonogramBoard", on_delete=models.SET_DEFAULT, null=True, default=None)
-    board = models.TextField(null=True)
-
-
 class History(models.Model):
     current_session = models.ForeignKey("Session", on_delete=models.CASCADE)
     gameplay_id = models.IntegerField()
@@ -52,3 +49,59 @@ class History(models.Model):
                 name="(session, game, turn) tuple"
             ),
         ]
+
+
+class Session(models.Model):
+    session_id = models.IntegerField(primary_key=True)
+    current_game = models.ForeignKey("History", on_delete=models.SET_DEFAULT, null=True, default=None)
+    board_data = models.ForeignKey("NonogramBoard", on_delete=models.SET_DEFAULT, null=True, default=None)
+    board = models.TextField(null=True)
+
+    def mark(
+        self,
+        x: int,
+        y: int,
+        new_state: GameBoardCellState,
+    ) -> bool:
+        board_data = self.board_data
+        current_game = self.current_game
+
+        if current_game is None or board_data is None:
+            return False
+        
+        num_row = board_data.num_row
+        num_column = board_data.num_column
+
+        if not (0 <= x < num_row) or not (0 <= y < num_column):
+            return False
+
+        board = deserialize_gameboard(board_data.board)
+        play = deserialize_gameplay(self.board)
+
+        current_cell_state = play[x][y]
+        current_cell = board[x][y]
+        changed = False
+
+        if current_cell_state == GameBoardCellState.REVEALED:
+            return False
+        if new_state == GameBoardCellState.REVEALED:
+            if current_cell == RealBoardCellState.BLACK:
+                changed = True
+        elif current_cell_state != new_state:
+            changed = True
+
+        if changed:
+            play[x][y] = new_state
+            new_history = History(
+                current_session=self,
+                gameplay_id=current_game.gameplay_id,
+                current_turn=current_game.current_turn + 1,
+                type_of_move=int(new_state),
+                x_coord=x,
+                y_coord=y,
+            )
+            new_history.save()
+            self.current_game = new_history
+            self.board = serialize_gameplay(play)
+            self.save()
+        return changed
