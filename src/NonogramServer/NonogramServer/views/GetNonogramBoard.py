0442786e26f33b01b62690bcdbc19e9403274ccd
kig9981 <ingu9981@gmail.com>
2024-06-04 22:12:15 +0900

    fix: GetNonogramBoard과 테스트코드 수정

--- src/NonogramServer/NonogramServer/views/GetNonogramBoard.py
+++ src/NonogramServer/NonogramServer/views/GetNonogramBoard.py
@@ -45,34 +45,8 @@ class GetNonogramBoard(AsyncAPIView):
     async def get(
         self,
         request: HttpRequest,
+        board_id: str,
     ) -> HttpResponse:
-        return HttpResponse("get_nonogram_board(get)")
-
-    async def post(
-        self,
-        request: HttpRequest,
-    ) -> HttpResponse:
-        BOARD_ID_QUERY = 0
-        if request.content_type != "application/json":
-            return HttpResponseBadRequest("Must be Application/json request.")
-        query = json.loads(request.body)
-        if 'session_id' not in query:
-            return HttpResponseBadRequest("session_id is missing.")
-        session_id = query['session_id']
-        if session_id == BOARD_ID_QUERY:
-            return await self._process_board_query(query)
-        else:
-            return await self._process_gameplay_query(query)
-
-    async def _process_board_query(
-        self,
-        query: dict,
-    ) -> HttpResponse:
-        if 'board_id' not in query:
-            return HttpResponseBadRequest("board_id is missing.")
-
-        board_id = query['board_id']
-
         if not isinstance(board_id, str) or not is_uuid4(board_id):
             return HttpResponseBadRequest(f"board_id '{board_id}' is not valid id.")
 
@@ -89,83 +63,6 @@ class GetNonogramBoard(AsyncAPIView):
             "board": deserialize_gameboard(board_data.board),
             "num_row": board_data.num_row,
             "num_column": board_data.num_column,
-        }
-
-        return JsonResponse(response_data)
-
-    async def _process_gameplay_query(
-        self,
-        query: dict,
-    ) -> HttpResponse:
-        GAME_NOT_START = 0
-        LATEST_TURN = -1
-        if 'game_turn' not in query:
-            return HttpResponseBadRequest("game_turn is missing.")
-        session_id = query['session_id']
-        game_turn = query['game_turn']
-        if not isinstance(session_id, str) or not is_uuid4(session_id):
-            return HttpResponseBadRequest(f"session_id '{session_id}' is not valid id.")
-
-        try:
-            session = await async_get_from_db(
-                model_class=Session,
-                label=f"session_id '{session_id}'",
-                select_related=['board_data', 'current_game'],
-                session_id=session_id,
-            )
-        except ObjectDoesNotExist as error:
-            return HttpResponseNotFound(f"{error} not found.")
-
-        board_data = session.board_data
-
-        if game_turn == GAME_NOT_START:
-            if board_data is None:
-                return HttpResponseNotFound("board not found.")
-            response_data = {
-                "board_id": str(board_data.board_id),
-                "board": board_data.board,
-                "num_row": board_data.num_row,
-                "num_column": board_data.num_column,
-            }
-
-            return JsonResponse(response_data)
-
-        latest_turn_info = session.current_game
-        latest_turn = 0 if latest_turn_info is None else latest_turn_info.current_turn
-
-        if not isinstance(game_turn, int) or not (-1 <= game_turn <= latest_turn):
-            return HttpResponseBadRequest(f"invalid game_turn. must be between 0 to {latest_turn}(latest turn)")
-
-        if game_turn == latest_turn or game_turn == LATEST_TURN:
-            if board_data is None:
-                return HttpResponseNotFound("board not found.")
-            board = deserialize_gameplay(
-                serialized_string=session.board,
-                return_int=True,
-            )
-        else:
-            gameplay = NonogramGameplay(
-                data=board_data,
-                db_sync=False,
-            )
-
-            async for move in History.objects.filter(
-                gameplay_id=latest_turn_info.gameplay_id,
-                current_turn__lte=game_turn,
-            ).order_by("current_turn"):
-                gameplay.mark(
-                    x=move.x_coord,
-                    y=move.y_coord,
-                    new_state=move.type_of_move,
-                )
-
-            board = gameplay.playboard
-
-        response_data = {
-            "board": board,
-            "num_row": board_data.num_row,
-            "num_column": board_data.num_column,
-            "latest_turn": latest_turn,
         }
 
         return JsonResponse(response_data)
