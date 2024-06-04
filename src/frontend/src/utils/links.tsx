dd352e93c86410f5d244396b6ce2978da13c6a75
강인구 <ingu9981@gmail.com>
2024-06-05 02:46:49 +0900

    fix: link.tsx 수정

--- src/frontend/src/utils/links.tsx
+++ src/frontend/src/utils/links.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
-export const api_server =  {
-    api_server_url: '${process.env.REACT_APP_API_SERVER_PROTOCOL}://${process.env.REACT_APP_API_SERVER_HOST}:${REACT_APP_API_SERVER_PORT}'
-}
+const api_server_protocol = process.env.REACT_APP_API_SERVER_PROTOCOL;
+const api_server_host = process.env.REACT_APP_API_SERVER_HOST;
+const api_server_port = process.env.REACT_APP_API_SERVER_PORT;
+
+export const api_server_url = api_server_protocol + '://' + 'localhost' + ':' + api_server_port;
