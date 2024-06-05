170d9731cf3f00e5c14de5ec597df1f8e6c20941
kig9981 <ingu9981@gmail.com>
2024-06-06 00:51:32 +0900

    feat: NonogramServer에서 기본 보드 추가하도록 수정

--- src/NonogramServer/NonogramServer/apps.py
+++ src/NonogramServer/NonogramServer/apps.py
@@ -1,6 +1,53 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+from .models import NonogramBoard
+from utils import serialize_gameboard
+from utils import RealBoardCellState
+import uuid
+
+default_board = [
+    [0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0],
+    [0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0],
+    [0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0],
+    [1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1],
+    [1, 1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0],
+    [1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0],
+    [0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1],
+    [1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1],
+    [0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1],
+    [0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0],
+    [0, 0, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0],
+    [0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1],
+    [0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1],
+    [0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 0, 1, 1],
+    [0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1],
+    [1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0],
+    [1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0],
+    [0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1],
+    [0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1],
+    [1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1]
+]
+
+def add_default_data(sender, **kwargs):
+    if not NonogramBoard.objects.exists():
+        NonogramBoard.objects.create(
+            board_id=str(uuid.uuid4),
+            board=serialize_gameboard(default_board),
+            num_row=len(default_board),
+            num_column=len(default_board[0]),
+            black_counter=sum(
+                sum(1 for item in row if item == RealBoardCellState.BLACK)
+                for row in default_board
+            ),
+            theme="default",
+        )
+
+
 
 
 class NonogramserverConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'NonogramServer'
+
+    def ready(self):
+        post_migrate.connect(my_callback, sender=self)
