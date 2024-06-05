778a7a70b280b2a747eb16627267587eca95af50
강인구 <ingu9981@gmail.com>
2024-06-06 01:45:54 +0900

    feat: 백엔드와 통신해서 게임 시작하는 로직 구현

--- src/frontend/src/components/GameBoard.tsx
+++ src/frontend/src/components/GameBoard.tsx
@@ -6,139 +6,159 @@ const REVEALED = 1;
 const MARK_X = 2;
 const MARK_QUESTION = 3;
 
-type CellState = 0 | 1 | 2 | 3;
-type BoardState = CellState[][];
+const WHITE = 0;
+const BLACK = 1;
+
+type PlayCellState = 0 | 1 | 2 | 3;
+type GameCellState = 0 | 1;
+type PlayBoardState = PlayCellState[][];
+type GameBoardState = GameCellState[][];
+
+const CellStateToStr = [
+    "",
+    "marked",
+    "x",
+    "question"
+]
+
+
+export type { GameCellState };
+export type { GameBoardState };
 
 interface GameBoardProps {
-  numRow: number;
-  numCol: number;
+    gameBoard: GameBoardState;
 }
 
-const GameBoard: React.FC<GameBoardProps> = ({ numRow, numCol }) => {
-  const initialBoard: BoardState = Array(numRow).fill(null).map(() => Array(numCol).fill(NOT_SELECTED));
-  const [board, setBoard] = useState<BoardState>(initialBoard);
-  const [rowHints, setRowHints] = useState<number[][]>([]);
-  const [colHints, setColHints] = useState<number[][]>([]);
+const GameBoard: React.FC<GameBoardProps> = ({ gameBoard }) => {
+    const numRow = gameBoard.length;
+    const numCol = gameBoard[0].length;
+    const initialBoard: PlayBoardState = Array(numRow).fill(null).map(() => Array(numCol).fill(NOT_SELECTED));
+    const [board, setBoard] = useState<PlayBoardState>(initialBoard);
+    const [rowHints, setRowHints] = useState<number[][]>([]);
+    const [colHints, setColHints] = useState<number[][]>([]);
 
-  useEffect(() => {
-    setRowHints(generateHints(initialBoard, 'row'));
-    setColHints(generateHints(initialBoard, 'col'));
-  }, []);
+    useEffect(() => {
+        setRowHints(generateHints(gameBoard, 'row'));
+        setColHints(generateHints(gameBoard, 'col'));
+    }, []);
 
-  const generateHints = (board: BoardState, type: 'row' | 'col'): number[][] => {
-    const hints: number[][] = [];
-    if (type === 'row') {
-      for (let row of board) {
-        const hint: number[] = [];
-        let count = 0;
-        for (let cell of row) {
-          if (cell === REVEALED) {
-            count++;
-          } else if (count > 0) {
-            hint.push(count);
-            count = 0;
-          }
+    const generateHints = (board: GameBoardState, type: 'row' | 'col'): number[][] => {
+        const hints: number[][] = [];
+        if (type === 'row') {
+        for (let row of board) {
+            const hint: number[] = [];
+            let count = 0;
+            for (let cell of row) {
+                if (cell === REVEALED) {
+                    count++;
+                } else if (count > 0) {
+                    hint.push(count);
+                    count = 0;
+                }
+            }
+            if (count > 0) hint.push(count);
+            hints.push(hint.reverse());
         }
-        if (count > 0) hint.push(count);
-        hints.push(hint.reverse());
-      }
-    } else if (type === 'col') {
-      for (let col = 0; col < board[0].length; col++) {
-        const hint: number[] = [];
-        let count = 0;
-        for (let row = 0; row < board.length; row++) {
-          if (board[row][col] === REVEALED) {
-            count++;
-          } else if (count > 0) {
-            hint.push(count);
-            count = 0;
-          }
+        } else if (type === 'col') {
+        for (let col = 0; col < board[0].length; col++) {
+            const hint: number[] = [];
+            let count = 0;
+            for (let row = 0; row < board.length; row++) {
+                if (board[row][col] === REVEALED) {
+                    count++;
+                } else if (count > 0) {
+                    hint.push(count);
+                    count = 0;
+                }
+            }
+            if (count > 0) hint.push(count);
+            hints.push(hint.reverse());
         }
-        if (count > 0) hint.push(count);
-        hints.push(hint.reverse());
-      }
-    }
-    return hints;
-  };
+        }
+        return hints;
+    };
 
-  const handleLeftClick = (row: number, col: number) => {
-    const newBoard = board.map((r, rowIndex) =>
-      r.map((c, colIndex) => {
-        if (rowIndex === row && colIndex === col) {
-          return c = REVEALED;
-        }
-        return c;
-      })
+    const handleLeftClick = (row: number, col: number) => {
+        const newBoard = board.map((r, rowIndex) =>
+            r.map((c, colIndex) => {
+                if (rowIndex === row && colIndex === col) {
+                    if (gameBoard[rowIndex][colIndex] == BLACK) {
+                        alert("Correct!");
+                        return c = REVEALED;
+                    }
+                    else {
+                        alert("Incorrect!");
+                        return c;
+                    }
+                }
+                return c;
+            })
+        );
+        setBoard(newBoard);
+    };
+
+    const handleRightClick = (row: number, col: number, e: MouseEvent) => {
+        e.preventDefault();
+        const newBoard = board.map((r, rowIndex) =>
+            r.map((c, colIndex) => {
+                if (rowIndex === row && colIndex === col) {
+                    if (c === MARK_X) {
+                        return MARK_QUESTION;
+                    } else if (c === MARK_QUESTION) {
+                        return NOT_SELECTED;
+                    } else if (c === NOT_SELECTED) {
+                        return MARK_X;
+                    }
+                }
+                return c;
+            })
+        );
+        setBoard(newBoard);
+    };
+
+    return (
+        <div className="game-board-container">
+            <div className="unused"> </div>
+            <div className="hints-row">
+                {colHints.map((hint, index) => (
+                    <div key={index} className="hint-col">
+                        {hint.map((num, i) => (
+                            <div key={i} className="hint-num">{num}</div>
+                        ))}
+                    </div>
+                ))}
+            </div>
+            <div className="hints-column">
+                {rowHints.map((hint, index) => (
+                    <div key={index} className="hint-row">
+                        {hint.map((num, i) => (
+                            <span key={i} className="hint-num">{num}</span>
+                        ))}
+                    </div>
+                ))}
+            </div>
+            <div className="game-board">
+                {board.map((row, rowIndex) => (
+                    <div key={rowIndex} className="game-row">
+                        {row.map((cell, colIndex) => (
+                            <div
+                                key={colIndex}
+                                onClick={() => handleLeftClick(rowIndex, colIndex)}
+                                onContextMenu={(e) => handleRightClick(rowIndex, colIndex, e)}
+                                className={`game-cell ${CellStateToStr[cell]}`}
+                            >
+                                {cell !== REVEALED && cell !== NOT_SELECTED && (
+                                    <span className={`cell-content ${CellStateToStr[cell]}`}>
+                                        {cell === MARK_X ? 'X' : cell === MARK_QUESTION ? '?' : ''}
+                                    </span>
+                                )}
+                            </div>
+                        ))}
+                    </div>
+                ))}
+            </div>
+        </div>
     );
-    setBoard(newBoard);
-    setRowHints(generateHints(newBoard, 'row'));
-    setColHints(generateHints(newBoard, 'col'));
-  };
-
-  const handleRightClick = (row: number, col: number, e: MouseEvent) => {
-    e.preventDefault();
-    const newBoard = board.map((r, rowIndex) =>
-      r.map((c, colIndex) => {
-        if (rowIndex === row && colIndex === col) {
-          if (c === MARK_X) {
-            return MARK_QUESTION;
-          } else if (c === MARK_QUESTION) {
-            return NOT_SELECTED;
-          } else if (c === NOT_SELECTED) {
-            return MARK_X;
-          }
-        }
-        return c;
-      })
-    );
-    setBoard(newBoard);
-    setRowHints(generateHints(newBoard, 'row'));
-    setColHints(generateHints(newBoard, 'col'));
-  };
-
-  return (
-    <div className="game-board-container">
-      <div className="unused"> </div>
-      <div className="hints-row">
-        {colHints.map((hint, index) => (
-          <div key={index} className="hint-col">
-            {hint.map((num, i) => (
-              <div key={i} className="hint-num">{num}</div>
-            ))}
-          </div>
-        ))}
-      </div>
-      <div className="hints-column">
-        {rowHints.map((hint, index) => (
-          <div key={index} className="hint-row">
-            {hint.map((num, i) => (
-              <span key={i} className="hint-num">{num}</span>
-            ))}
-          </div>
-        ))}
-      </div>
-      <div className="game-board">
-        {board.map((row, rowIndex) => (
-          <div key={rowIndex} className="game-row">
-            {row.map((cell, colIndex) => (
-              <div
-                key={colIndex}
-                onClick={() => handleLeftClick(rowIndex, colIndex)}
-                onContextMenu={(e) => handleRightClick(rowIndex, colIndex, e)}
-                className={`game-cell ${cell}`}
-              >
-                {cell !== REVEALED && cell !== NOT_SELECTED && (
-                  <span className={`cell-content ${cell}`}>
-                    {cell === MARK_X ? 'X' : cell === MARK_QUESTION ? '?' : ''}
-                  </span>
-                )}
-              </div>
-            ))}
-          </div>
-        ))}
-      </div>
-    </div>
-  );
 };
 
 export default GameBoard;
