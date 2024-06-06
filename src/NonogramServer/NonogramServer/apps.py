c236abea99d0ba63898f138593c552ef3469db69
kig9981 <ingu9981@gmail.com>
2024-06-06 19:29:21 +0900

    feat: default data 한개더 추가

--- src/NonogramServer/NonogramServer/apps.py
+++ src/NonogramServer/NonogramServer/apps.py
@@ -27,6 +27,19 @@ default_board = [
     [0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0]
 ]
 
+default_board2 = [
+    [1, 0, 1, 0, 0, 1, 1, 0, 0, 0],
+    [1, 0, 0, 1, 0, 1, 0, 1, 1, 1],
+    [1, 1, 0, 0, 1, 0, 1, 1, 0, 1],
+    [1, 0, 0, 1, 0, 0, 0, 1, 0, 0],
+    [0, 0, 1, 0, 1, 1, 1, 0, 0, 1],
+    [0, 0, 0, 0, 1, 1, 1, 1, 1, 0],
+    [0, 0, 1, 0, 1, 1, 1, 0, 1, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 0, 1, 0, 1, 1, 0, 0],
+    [1, 1, 1, 0, 1, 1, 1, 0, 1, 1]
+]
+
 
 def add_default_data(sender, **kwargs):
     from .models import NonogramBoard
@@ -39,6 +52,18 @@ def add_default_data(sender, **kwargs):
             black_counter=sum(
                 sum(1 for item in row if item == RealBoardCellState.BLACK)
                 for row in default_board
+            ),
+            theme="default",
+        )
+
+        NonogramBoard.objects.create(
+            board_id=str(uuid.uuid4()),
+            board=serialize_gameboard(default_board2),
+            num_row=len(default_board2),
+            num_column=len(default_board2[0]),
+            black_counter=sum(
+                sum(1 for item in row if item == RealBoardCellState.BLACK)
+                for row in default_board2
             ),
             theme="default",
         )
