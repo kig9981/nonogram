6a94eb9a8c05492b90b510c3a104261b389b0eca
kig9981 <ingu9981@gmail.com>
2024-06-12 22:58:52 +0900

    dockerfile 통합

--- docker_build_develop.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker build -f docker/develop/Dockerfile.NonogramServer -t nonogram_server:${1:-latest} .
-docker build -f docker/develop/Dockerfile.ApiServer -t api_server:${1:-latest} .
-docker build -f docker/develop/Dockerfile.Frontend -t frontend_server:${1:-latest} .
-docker image prune -f
