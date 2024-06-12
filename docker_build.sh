298acfa093eed7fc9736e3b5d6a4b5bab4e4bbee
강인구 <ingu9981@gmail.com>
2024-06-12 22:58:52 +0900

    dockerfile 통합

---
+++ docker_build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker build -f docker/Dockerfile.NonogramServer -t nonogram_server:${1:-latest} .
+docker build -f docker/Dockerfile.ApiServer -t api_server:${1:-latest} .
+docker build -f docker/Dockerfile.Frontend -t frontend_server:${1:-latest} .
+docker image prune -f
