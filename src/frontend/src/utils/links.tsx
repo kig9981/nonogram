ce319d4aa232580213b980bef4d3bc75b709630f
kig9981 <ingu9981@gmail.com>
2024-07-29 16:35:30 +0900

    fix: 프론트가 api server domain을 통해 통신하도록 수정

--- src/frontend/src/utils/links.tsx
+++ src/frontend/src/utils/links.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const api_server_protocol = process.env.REACT_APP_API_SERVER_PROTOCOL;
-const api_server_host = process.env.REACT_APP_API_SERVER_HOST;
-const api_server_port = process.env.REACT_APP_API_SERVER_PORT;
+const api_server_domain = process.env.REACT_APP_API_SERVER_DOMAIN;
 
-export const api_server_url = api_server_protocol + '://' + api_server_host + ':' + api_server_port;
+export const api_server_url = api_server_protocol + '://' + api_server_domain + '/api';
