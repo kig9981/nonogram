dcb9eac4c524b1dd857691851e32af474d28ba6b
kig9981 <ingu9981@gmail.com>
2024-07-29 17:30:04 +0900

    fix: link.tsx 수정

--- src/frontend/src/utils/links.tsx
+++ src/frontend/src/utils/links.tsx
@@ -1,6 +1,4 @@
-import React from 'react';
-
 const api_server_protocol = process.env.REACT_APP_API_SERVER_PROTOCOL;
 const api_server_domain = process.env.REACT_APP_API_SERVER_DOMAIN;
 
-export const api_server_url = api_server_protocol + '://' + api_server_domain + '/api';
+export const api_server_url = `${api_server_protocol}://${api_server_domain}/api`;
