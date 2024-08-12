26c76779291e2f731cade5a37a4b699874ba8e9e
kig9981 <ingu9981@gmail.com>
2024-08-12 23:53:49 +0900

    fix: 서버 실행 스크립트 수정

--- run_develop_server.sh
+++ run_develop_server.sh
@@ -4,7 +4,14 @@ HOST=${1:-"localhost"}
 
 echo "HOST: ${HOST}"
 
+export $(grep -v '^#' .env | xargs)
+
+mkdir -p ./prometheus/.temp/
+rm -f ./prometheus/.temp/prometheus.yaml
+
 envsubst < ./prometheus/prometheus.yaml.template > ./prometheus/.temp/prometheus.yaml
+
+mkdir -p ./grafana
 
 export HOST=$HOST
 docker compose -f docker-compose.develop.yaml up
