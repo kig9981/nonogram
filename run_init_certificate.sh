ddcc2ad7176bc2eac00ab91dfdacdbec74af17aa
강인구 <ingu9981@gmail.com>
2024-08-14 21:41:08 +0900

    fix: 오타수정

--- run_init_certificate.sh
+++ run_init_certificate.sh
@@ -4,4 +4,4 @@ export $(grep -v '^#' .env | xargs)
 
 mkdir -p ./certbot
 
-docker compose -f docker-compose.ssl-certificate.yaml up
+docker compose -f docker-compose.ssl-certificate.yaml up --exit-code-from certbot
