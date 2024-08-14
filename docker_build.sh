383f217b74d4863034937fcf7357a0a085df284a
강인구 <ingu9981@gmail.com>
2024-08-14 19:38:29 +0900

    docker: python빌드를 이미지로 분리

--- docker_build.sh
+++ docker_build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-docker build -f docker/Dockerfile.NonogramServer -t nonogram_server:${1:-latest} .
-docker build -f docker/Dockerfile.ApiServer -t api_server:${1:-latest} .
+docker build -f docker/Dockerfile.PythonEnv -t common_base_python:${1:-latest} .
+docker build --build-arg VERSION=${1:-latest} -f docker/Dockerfile.NonogramServer -t nonogram_server:${1:-latest} .
+docker build --build-arg VERSION=${1:-latest} -f docker/Dockerfile.ApiServer -t api_server:${1:-latest} .
 docker build -f docker/Dockerfile.Frontend -t frontend_server:${1:-latest} .
 docker image prune -f
