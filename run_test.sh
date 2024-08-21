f48841881cd43ef927a1f49bf562905448327e69
kig9981 <ingu9981@gmail.com>
2024-08-21 14:47:38 +0900

    script: 부하테스트 스크립트 추가

--- run_test.sh
+++ run_test.sh
@@ -3,3 +3,6 @@
 pytest tests/unit/test_api_server
 pytest tests/unit/test_nonogram_server
 pytest tests/integration
+
+export $(grep -v '^#' tests/test.env | xargs)
+locust -f tests/performance/test_nonogram_server/CreateNewSession.py --headless -u 100 --run-time 60s -H http://localhost:${NONOGRAM_SERVER_PORT} --processes 100
