3f01960e9593e1b2435490889b229b257ca1c4a4
kig9981 <ingu9981@gmail.com>
2024-08-24 15:25:46 +0900

    fix: locust 유저수 조정

--- run_test.sh
+++ run_test.sh
@@ -5,4 +5,4 @@ pytest tests/unit/test_nonogram_server
 pytest tests/integration
 
 export $(grep -v '^#' tests/test.env | xargs)
-locust -f tests/performance/test_nonogram_server/CreateNewSession.py --headless -u 100 --run-time 60s -H http://localhost:${NONOGRAM_SERVER_PORT} --processes 100
+locust -f tests/performance/test_nonogram_server/CreateNewSession.py --headless -u 10 --run-time 60s -H http://localhost:${NONOGRAM_SERVER_PORT} --processes 10
