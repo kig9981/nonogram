83395dd53bf314157c25a989e0687c81d34e88c1
kig9981 <ingu9981@gmail.com>
2024-08-25 00:37:23 +0900

    script: 부하테스트 스크립트 수정

--- run_performance_test.sh
+++ run_performance_test.sh
@@ -7,11 +7,11 @@ echo "test condition = $test_condition"
 if [[ $test_condition == "local" ]]; then
     export $(grep -v '^#' tests/test.env | xargs)
     export TEST_CONDITION="local"
-    locust -f tests/performance/test_nonogram_server/CreateNewSession.py --headless -u 10 --run-time 60s -H http://localhost:${NONOGRAM_SERVER_PORT} --processes 10
+    locust -f tests/performance/test_nonogram_server/CreateNewSession.py --headless -u 100 --run-time 60s -H http://localhost:${NONOGRAM_SERVER_PORT} --processes 5
 elif [[ $test_condition == "production" ]]; then
     export $(grep -v '^#' .env | xargs)
     export TEST_CONDITION="production"
-    locust -f tests/performance/test_nonogram_server/CreateNewSession.py --headless -u 10 --run-time 60s -H ${SERVER_PROTOCOL}://${SERVER_DOMAIN}/api --processes 10
+    locust -f tests/performance/test_nonogram_server/CreateNewSession.py --headless -u 100 --run-time 60s -H ${SERVER_PROTOCOL}://${SERVER_DOMAIN}/api --processes 5
 else
     echo "Invalid test condition"
 fi
