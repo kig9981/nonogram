c1f46c8d047394f9f7f1737fe254f53ef748aed8
kig9981 <ingu9981@gmail.com>
2024-08-24 17:26:49 +0900

    script: 부하테스트를 배포환경에서 돌리는 세팅 추가

--- run_performance_test.sh
+++ run_performance_test.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
-export $(grep -v '^#' tests/test.env | xargs)
-locust -f tests/performance/test_nonogram_server/CreateNewSession.py --headless -u 10 --run-time 60s -H http://localhost:${NONOGRAM_SERVER_PORT} --processes 10
+test_condition=${1:-local}
+
+echo "test condition = $test_condition"
+
+if [[ $test_condition == "local" ]]; then
+    export $(grep -v '^#' tests/test.env | xargs)
+    export TEST_CONDITION="local"
+    locust -f tests/performance/test_nonogram_server/CreateNewSession.py --headless -u 10 --run-time 60s -H http://localhost:${NONOGRAM_SERVER_PORT} --processes 10
+elif [[ $test_condition == "production" ]]; then
+    export $(grep -v '^#' .env | xargs)
+    export TEST_CONDITION=""
+    locust -f tests/performance/test_nonogram_server/CreateNewSession.py --headless -u 10 --run-time 60s -H ${SERVER_PROTOCOL}://${SERVER_DOMAIN}/api --processes 10
+else
+    echo "Invalid test condition"
+fi
--- run_test.sh
+++ run_test.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+test_condition=${1:-local}
+
 pytest tests/unit/test_api_server
 pytest tests/unit/test_nonogram_server
 pytest tests/integration
 
-export $(grep -v '^#' tests/test.env | xargs)
-locust -f tests/performance/test_nonogram_server/CreateNewSession.py --headless -u 10 --run-time 60s -H http://localhost:${NONOGRAM_SERVER_PORT} --processes 10
+./run_performance_test.sh $test_condition
