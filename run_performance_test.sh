14bb55913553da5612b88c1a5ea2901445899f1e
강인구 <ingu9981@gmail.com>
2024-08-26 01:29:10 +0900

    tests: 부하테스트 작성

--- run_performance_test.sh
+++ run_performance_test.sh
@@ -9,11 +9,13 @@ if [[ $test_condition == "local" ]]; then
     export TEST_CONDITION="local"
     locust -f tests/performance/CreateNewSession.py --headless -u 100 --run-time 60s -H http://localhost:${API_SERVER_PORT} --processes 5
     locust -f tests/performance/CreateNewGame.py --headless -u 100 --run-time 60s -H http://localhost:${API_SERVER_PORT} --processes 5
+    locust -f tests/performance/GamePlay.py --headless -u 100 --run-time 60s -H http://localhost:${API_SERVER_PORT} --processes 5
 elif [[ $test_condition == "production" ]]; then
     export $(grep -v '^#' .env | xargs)
     export TEST_CONDITION="production"
     locust -f tests/performance/CreateNewSession.py --headless -u 100 --run-time 60s -H ${SERVER_PROTOCOL}://${SERVER_DOMAIN}/api --processes 5
     locust -f tests/performance/CreateNewGame.py --headless -u 100 --run-time 60s -H ${SERVER_PROTOCOL}://${SERVER_DOMAIN}/api --processes 5
+    locust -f tests/performance/GamePlay.py --headless -u 100 --run-time 60s -H ${SERVER_PROTOCOL}://${SERVER_DOMAIN}/api --processes 5
 else
     echo "Invalid test condition"
 fi
