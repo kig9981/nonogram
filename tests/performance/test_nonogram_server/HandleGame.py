8b7a5a229ae8478f1107f9925546a30362b299e8
강인구 <ingu9981@gmail.com>
2024-08-25 21:19:23 +0900

    tests: CreateNewGame 부하테스트 추가

---
+++ tests/performance/test_nonogram_server/HandleGame.py
@@ -0,0 +1,48 @@
+from locust import HttpUser
+from locust import task
+from locust import between
+from json import JSONDecodeError
+import random
+import tests.performance.config
+
+
+RANDOM_BOARD = 0
+GAME_EXIST = 0
+NEW_GAME_STARTED = 1
+
+response_code_to_str = [
+    "GAME_EXIST",
+    "NEW_GAME_STARTED",
+]
+
+
+class HandleGameUser(HttpUser):
+    wait_time = between(0.5, 2)
+    user_cnt = 1
+
+    def on_start(self) -> None:
+        self.user_num = HandleGameUser.user_cnt
+        HandleGameUser.user_cnt += 1
+        with self.client.post("/sessions", json={"client_session_key": f"0.0.0.0_test-agent{self.user_num}"}, catch_response=True) as response:
+            try:
+                self.session_id = response.json()["session_id"]
+                print(f"Sucessfully got session_id: {self.session_id}")
+            except JSONDecodeError:
+                response.failure("Response could not be decoded as JSON")
+            except KeyError:
+                response.failure("Response did not contain expected key 'session_id'")
+        return super().on_start()
+
+    @task
+    def create_new_game(self):
+        with self.client.put(f"/sessions/{self.session_id}", json={"board_id": RANDOM_BOARD}, catch_response=True) as response:
+            try:
+                board_id = response.json()["board_id"]
+                response_code = response.json()["response"]
+                
+                print(f"Sucessfully got response({response_code_to_str[response_code]}): {board_id}")
+            except JSONDecodeError:
+                response.failure("Response could not be decoded as JSON")
+            except KeyError:
+                response.failure("Response did not contain expected key 'session_id'")
+
