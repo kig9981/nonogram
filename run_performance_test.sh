8bd97a5ab2a0674b3006bbcd15436aeb48ce22b3
강인구 <ingu9981@gmail.com>
2024-08-25 21:19:41 +0900

    script: 추가한 부하테스트의 실행 스크립트 작성

--- run_performance_test.sh
+++ run_performance_test.sh
@@ -8,10 +8,12 @@ if [[ $test_condition == "local" ]]; then
     export $(grep -v '^#' tests/test.env | xargs)
     export TEST_CONDITION="local"
     locust -f tests/performance/test_nonogram_server/CreateNewSession.py --headless -u 100 --run-time 60s -H http://localhost:${NONOGRAM_SERVER_PORT} --processes 5
+    locust -f tests/performance/test_nonogram_server/HandleGame.py --headless -u 100 --run-time 60s -H http://localhost:${NONOGRAM_SERVER_PORT} --processes 5
 elif [[ $test_condition == "production" ]]; then
     export $(grep -v '^#' .env | xargs)
     export TEST_CONDITION="production"
     locust -f tests/performance/test_nonogram_server/CreateNewSession.py --headless -u 100 --run-time 60s -H ${SERVER_PROTOCOL}://${SERVER_DOMAIN}/api --processes 5
+    locust -f tests/performance/test_nonogram_server/HandleGame.py --headless -u 100 --run-time 60s -H ${SERVER_PROTOCOL}://${SERVER_DOMAIN}/api --processes 5
 else
     echo "Invalid test condition"
 fi
